20b047eb4fca08184f126d8eda6088a9de1ddd8e
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-06-12 16:18:36 +0700

    tesssss

--- tests/heading_test.py
+++ tests/heading_test.py
@@ -12,24 +12,39 @@ class BatikWebsiteTestCase(unittest.TestCase):
         option.add_argument('--headless')  # Menjalankan browser tanpa antarmuka grafis
         cls.browser = webdriver.Firefox(options=option)
 
-        cls.url = os.getenv('URL', 'http://localhost')  # Mengambil URL dari variabel lingkungan atau default ke 'http://localhost'
+        try:
+            cls.url = os.environ['URL']  # Mengambil URL dari variabel lingkungan
+        except:
+            cls.url = "http://localhost"
 
-    def test_1_page_heading_check(self):  
-        access_url = f'{self.url}/index.php'
+    def test(self):
+        # Menjalankan serangkaian pengujian
+        self.page_heading_check()
+        self.product_check()
+        self.contact_check()
+
+    def page_heading_check(self):  
+        access_url = 'http://' + self.url + '/index.php'
         self.browser.get(access_url)
-        heading_element = self.browser.find_element(By.TAG_NAME, 'h1') 
+
+        time.sleep(5)
+        heading_element = self.browser.find_element(By.TAG_NAME, 'h1')
         print("Heading text:", heading_element.text)
         self.assertIn('WELCOME TO\nSTORE BATIK TULIS', heading_element.text)  
 
-    def test_2_product_check(self):  
-        self.browser.get(f'{self.url}/index.php')
+    def product_check(self):  
+        access_url = 'http://' + self.url + '/index.php'
+        self.browser.get(access_url)
+
         time.sleep(5)
         self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[2]/a').click() 
         heading_element = self.browser.find_element(By.TAG_NAME, 'h2') 
         self.assertIn('~ ETALASE PRODUCT ~', heading_element.text)  
 
-    def test_3_contact_check(self):  
-        self.browser.get(f'{self.url}/index.php')
+    def contact_check(self):  
+        access_url = self.url + '/index.php'
+        self.browser.get(access_url)
+
         time.sleep(5)
         self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[3]/a').click() 
         heading_element = self.browser.find_element(By.TAG_NAME, 'h3') 
