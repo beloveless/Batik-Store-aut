29692b527e23be942591c2affb73cae38acd88b5
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-06-12 15:47:47 +0700

    cobaa

--- tests/heading_test.py
+++ tests/heading_test.py
@@ -1,4 +1,6 @@
-import unittest, os
+import unittest
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -10,34 +12,28 @@ class BatikWebsiteTestCase(unittest.TestCase):
         option.add_argument('--headless')  # Menjalankan browser tanpa antarmuka grafis
         cls.browser = webdriver.Firefox(options=option)
 
-        try:
-            cls.url = os.environ['URL']  # Mengambil URL dari variabel lingkungan
-        except:
-            cls.url = "http://localhost"
+        cls.url = os.getenv('URL', 'http://localhost')  # Mengambil URL dari variabel lingkungan atau default ke 'http://localhost'
 
-    def test(self):
-        # Menjalankan serangkaian pengujian
-        self.test_1_page_heading_check()
-        self.test_2_product_check()
-        self.test_3_contact_check()
-    
     def test_1_page_heading_check(self):  
-        access_url = self.url + '/index.php'
+        access_url = f'{self.url}/index.php'
         self.browser.get(access_url)
-        heading_element2 = self.browser.find_element(By.TAG_NAME, 'h1') 
-        print("Heading text:", heading_element2.text)
-        # Menghilangkan atau mengganti karakter '\n' dengan karakter baris baru
-        self.assertIn('WELCOME TO\nSTORE BATIK TULIS', heading_element2.text)  
+        heading_element = self.browser.find_element(By.TAG_NAME, 'h1') 
+        print("Heading text:", heading_element.text)
+        self.assertIn('WELCOME TO\nSTORE BATIK TULIS', heading_element.text)  
 
     def test_2_product_check(self):  
+        self.browser.get(f'{self.url}/index.php')
+        time.sleep(5)
         self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[2]/a').click() 
         heading_element = self.browser.find_element(By.TAG_NAME, 'h2') 
         self.assertIn('~ ETALASE PRODUCT ~', heading_element.text)  
 
     def test_3_contact_check(self):  
+        self.browser.get(f'{self.url}/index.php')
+        time.sleep(5)
         self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[3]/a').click() 
-        heading_element3 = self.browser.find_element(By.TAG_NAME, 'h3') 
-        self.assertIn('HUBUNGI KAMI :', heading_element3.text)  
+        heading_element = self.browser.find_element(By.TAG_NAME, 'h3') 
+        self.assertIn('HUBUNGI KAMI :', heading_element.text)  
     
     @classmethod
     def tearDownClass(cls):
