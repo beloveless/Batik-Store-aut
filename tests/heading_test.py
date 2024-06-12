493d377904bfcc9af638a6ef098c3d097290e63a
sodamnloveless <129027879+beloveless@users.noreply.github.com>
2024-06-12 15:18:30 +0700

    tess

---
+++ tests/heading_test.py
@@ -0,0 +1,40 @@
+import unittest, os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class BatikWebsiteTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Inisialisasi pengaturan untuk browser Firefox
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')  # Menjalankan browser tanpa antarmuka grafis
+        cls.browser = webdriver.Firefox(options=option)
+
+        try:
+            cls.url = os.environ['URL']  # Mengambil URL dari variabel lingkungan
+        except:
+            cls.url = "http://localhost"
+    
+    def test_1_page_heading_check(self):  
+        self.browser.get('http://localhost/Batik-Store-aut-master/src/index.php') 
+        heading_element2 = self.browser.find_element(By.TAG_NAME, 'h1') 
+        print("Heading text:", heading_element2.text)
+        # Menghilangkan atau mengganti karakter '\n' dengan karakter baris baru
+        self.assertIn('WELCOME TO\nSTORE BATIK TULIS', heading_element2.text)  
+
+    def test_2_product_check(self):  
+        self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[2]/a').click() 
+        heading_element = self.browser.find_element(By.TAG_NAME, 'h2') 
+        self.assertIn('~ ETALASE PRODUCT ~', heading_element.text)  
+
+    def test_3_contact_check(self):  
+        self.browser.find_element(By.XPATH, '//*[@id="navbarNav"]/ul/li[3]/a').click() 
+        heading_element3 = self.browser.find_element(By.TAG_NAME, 'h3') 
+        self.assertIn('HUBUNGI KAMI :', heading_element3.text)  
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, warnings='ignore')
